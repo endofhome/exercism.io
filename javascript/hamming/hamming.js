9a9bed97d95d6002f3d4a6be32d1a64b37259d9d
Tom Barnes <ruralmyth@gmail.com>
2016-02-06 14:01:27 +0000

    completed hamming

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,13 @@
+var Hamming = function() {};
+
+Hamming.prototype.compute = function(a, b) {
+  var hammingDistance = 0;
+  var i;
+  if (a.length !== b.length) { throw('DNA strands must be of equal length.'); }
+  for (i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) { hammingDistance++; }
+  }
+  return hammingDistance;
+};
+
+module.exports = Hamming;
