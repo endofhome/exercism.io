dce55647983aee26cfa1b98fb3dfcbd5c70db2ce
Tom Barnes <ruralmyth@gmail.com>
2016-02-07 18:09:04 +0000

    completed rna-transcription

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,14 @@
+var DnaTranscriber = function() {
+  this.mapDnaToRna = {'G':'C', 'C':'G', 'T':'A', 'A':'U'};
+};
+
+DnaTranscriber.prototype.toRna = function(dna) {
+  var rna = new String();
+  var i;
+  for (i = 0; i < dna.length; i++) {
+    rna = rna.concat(this.mapDnaToRna[dna[i]]);
+  };
+  return rna;
+};
+
+module.exports = DnaTranscriber;
