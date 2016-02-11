5e9cf84c0932590b93f2be966069c7f4359eca70
Tom Barnes <ruralmyth@gmail.com>
2016-02-11 01:25:57 +0000

    refractoring

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,8 @@
 var Hamming = function() {};
 
 Hamming.prototype.compute = function(a, b) {
-  var hammingDistance = 0;
-  var i;
+  var hammingDistance = 0,
+      i;
   if (a.length !== b.length) { throw new Error('DNA strands must be of equal length.'); }
   for (i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) { hammingDistance++; }
