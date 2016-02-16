727e32e638e8c360a3ae1c3c809cdf89a4e241f9
Tom Barnes <ruralmyth@gmail.com>
2016-02-16 00:05:35 +0000

    completed word-count

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,27 @@
+var Words = function() {};
+
+Words.prototype.count = function(string) {
+  return convertToHash(convertToArray(string));
+};
+  
+var convertToArray = function(string) {
+  var array = [];
+  array = string.trim().split(/\s+/);
+  console.log(array);
+  return array;
+};
+
+var convertToHash = function(array) {
+  var hash = Object.create(null);
+  for (i = 0; i < array.length; i++) {
+    var word = array[i];
+    if (!hash[word]) {
+      hash[word] = 1;
+    } else {
+      hash[word]++;
+    }
+  }
+  return hash;
+};
+
+module.exports = Words;
