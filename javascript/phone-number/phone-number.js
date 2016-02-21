44a57a5d7b05ed57a4495be461641ab321c478b1
Tom Barnes <ruralmyth@gmail.com>
2016-02-21 12:54:39 +0000

    invalid when 11 digits

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -6,8 +6,12 @@ PhoneNumber.prototype.number = function() {
   var result = 0,
       num = this.phoneNumber;
   result = num.replace(/[() -.]/g, "");
-  if ((result.length === 11) && (result.charAt(0) === '1')) {
-  result = result.substr(1);
+  if (result.length === 11) { 
+    if (result.charAt(0) === '1') {
+      result = result.substr(1);
+    } else {
+      result = '0000000000';
+    }
   }
   return result;
 };
