5240efb9a8a1681b2a1aabd9d168393d58c908b4
Tom Barnes <ruralmyth@gmail.com>
2016-02-23 14:55:53 +0000

    no matches

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,8 +2,23 @@ var Anagram = function(string) {
   this.string = string;
 };
 
-Anagram.prototype.matches = function() {
-  return [];
+Anagram.prototype.matches = function(testArray) {
+  var result = [],
+      i,
+      j;
+  for (i = 0; i < testArray.length; i++) {
+    for (j = 0; j < testArray[i].length; j++) {
+      var found = true;
+      if (this.string.indexOf(testArray[i].charAt(j)) === -1) {
+	found = false;
+	break;
+      };
+    }
+    if (j === (testArray[i].length -1) && (found === true)) {
+      result.push(testArray[i]);
+    };
+  }
+  return result;
 };
 
 module.exports = Anagram;
