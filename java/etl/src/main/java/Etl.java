b49e1d985e8242cfbc76a78e6e5d309d5b9eb327
Tom Barnes <ruralmyth@gmail.com>
2016-09-08 23:59:47 +0100

    nicer solution for Etl: testTransformOneValue

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,13 +1,14 @@
-import com.google.common.collect.ImmutableMap;
-
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Etl {
+
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-       Integer firstKey = old.keySet().iterator().next();
-       String firstValue = old.get(firstKey).get(0).toLowerCase();
-       ImmutableMap<String, Integer> result = ImmutableMap.of(firstValue, firstKey);
+       Map<String, Integer> result = new HashMap<String, Integer>();
+       String one = old.get(1).get(0).toLowerCase();
+       result.put(one, 1);
        return result;
    }
+
 }
