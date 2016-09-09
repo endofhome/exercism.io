14f42146033d142637370ae211829eb1a7b70a84
Tom Barnes <ruralmyth@gmail.com>
2016-09-09 23:31:33 +0100

    Java: Nucleotide-Count, first test passing

---
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DNA {
+
+    private final String identifier;
+
+    public DNA(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Integer count(char character) {
+        return 0;
+    }
+
+    public Map<Character, Integer> nucleotideCounts() {
+        return new HashMap<>();
+    }
+}
