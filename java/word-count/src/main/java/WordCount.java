75fa34ebec1210c19d9f6f5b730535426aeff913
Tom Barnes <ruralmyth@gmail.com>
2016-09-11 00:35:13 +0100

    Java: Word-Count, #3 countMultipleOccurences passing

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -6,8 +6,13 @@ public class WordCount {
     public Map<String,Integer> phrase(String phrase) {
         String[] words = phrase.split(" ");
         Map<String, Integer> wordCount = new HashMap<>();
+
         for (String word:words) {
-            wordCount.put(word, 1);
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
         }
         return wordCount;
     }
