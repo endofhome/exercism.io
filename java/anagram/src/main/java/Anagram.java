5c12b3daf0f8a64aea3d6289e595d5134e7de4ef
Tom Barnes <ruralmyth@gmail.com>
2016-09-12 00:37:22 +0100

    Java: Anagram, #1 testNoMatches passing

---
+++ java/anagram/src/main/java/Anagram.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Anagram {
+
+    public Anagram(String word) {
+    }
+
+    public List<String> match(List<String> strings) {
+        return new ArrayList<>();
+    }
+}
