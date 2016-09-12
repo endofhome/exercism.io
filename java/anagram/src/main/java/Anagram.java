52be733cbef69eee03b37f7b588b9717419c2e2e
Tom Barnes <ruralmyth@gmail.com>
2016-09-12 23:28:12 +0100

    Java: Anagram, tests #2-3 passing

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -3,10 +3,20 @@ import java.util.List;
 
 public class Anagram {
 
+    private final String baseWord;
+
     public Anagram(String word) {
+        this.baseWord = word;
     }
 
-    public List<String> match(List<String> strings) {
-        return new ArrayList<>();
+    public List<String> match(List<String> words) {
+        ArrayList<String> matches = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() != baseWord.length()) {
+                break;
+            }
+            matches.add(word);
+        }
+        return matches;
     }
 }
