ea463ff1e2ee3d8673b0c90ac35628a49445db70
Tom Barnes <ruralmyth@gmail.com>
2016-09-17 19:36:22 +0100

    Java: phone-number, #1 cleansNumber passing

---
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -0,0 +1,20 @@
+public class PhoneNumber {
+
+    private String number;
+
+    public PhoneNumber (String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return "1234567890";
+    }
+
+    public String getAreaCode() {
+        return null;
+    }
+
+    public String pretty() {
+        return null;
+    }
+}
