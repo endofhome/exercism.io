b8f0e13f28046020b1384a3770b52253c4240855
Tom Barnes <ruralmyth@gmail.com>
2016-12-21 23:55:54 +0000

    gigasecond: now using POSIX-compliant date utility

--- bash/gigasecond/gigasecond.sh
+++ bash/gigasecond/gigasecond.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ORIGIN=$1
-SECONDS=$(gdate --date="${ORIGIN} 1000000000 seconds" '+%s')
-RESULT=$(gdate -d "1970-01-01 UTC ${SECONDS} seconds" +'%a %b %e %H:%M:%S UTC %Y')
+SECONDS=$(date --date="${ORIGIN} 1000000000 seconds" '+%s')
+RESULT=$(date -d "1970-01-01 UTC ${SECONDS} seconds" +'%a %b %e %H:%M:%S UTC %Y')
 echo $RESULT
