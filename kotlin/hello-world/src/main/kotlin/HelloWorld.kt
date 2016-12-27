fb913283e8b43c645e3f0d256125e6584eb91ef5
Tom Barnes <ruralmyth@gmail.com>
2016-12-27 00:37:47 +0000

    Kotlin: hello-world, refactor into a slightly nicer shape

--- kotlin/hello-world/src/main/kotlin/HelloWorld.kt
+++ kotlin/hello-world/src/main/kotlin/HelloWorld.kt
@@ -1,12 +1,19 @@
 object HelloWorld {
-    fun hello(name: String? = "Default name"): String {
-        val processedName = when (name) {
-            "Default name" -> "World"
+    fun hello(input: String?): String {
+        val strippedName = stripNullOrBlank(input)
+        val processedName = when (strippedName) {
             null -> "World"
-            "" -> "World"
-            "    " -> "World"
-            else -> name
+            else -> strippedName
         }
         return "Hello, $processedName!"
+    }
+
+    fun hello(): String {
+        return hello("World")
+    }
+
+    private fun stripNullOrBlank(input: String?): String? {
+        if (input.isNullOrBlank()) return null
+        return input
     }
 }
