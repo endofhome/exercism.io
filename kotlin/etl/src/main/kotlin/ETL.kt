2bdae7fff6eb68b1b37ae5b227297bb5a1670d89
Tom Barnes <ruralmyth@gmail.com>
2017-01-02 23:33:44 +0000

    Kotlin: etl, test #1 passing for a better reason

--- kotlin/etl/src/main/kotlin/ETL.kt
+++ kotlin/etl/src/main/kotlin/ETL.kt
@@ -1,5 +1,6 @@
 object ETL {
     fun  transform(old: Map<Int, List<String>>): Map<String, Int> {
-        return mapOf("world" to 1)
+        val firstEntry = old.entries.first()
+        return mapOf(firstEntry.value.get(0).toLowerCase() to firstEntry.key)
     }
 }
