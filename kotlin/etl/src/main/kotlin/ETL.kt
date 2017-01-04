6a83d1a1ec74c66c140c95d405c67a9a51d91fd4
Tom Barnes <ruralmyth@gmail.com>
2017-01-04 21:32:15 +0000

    formatting

--- kotlin/etl/src/main/kotlin/ETL.kt
+++ kotlin/etl/src/main/kotlin/ETL.kt
@@ -1,7 +1,8 @@
 object ETL {
-    fun  transform(old: Map<Int, List<String>>): Map<String, Int> {
+    fun transform(old: Map<Int, List<String>>): Map<String, Int> {
         return old.entries.flatMap {
-            entry -> entry.value.map {
+            entry ->
+            entry.value.map {
                 it.toLowerCase() to entry.key
             }
         }.toMap()
