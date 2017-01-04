4c0dcd04c90fb5dd60d4e86d293fa389fccf2c7d
Tom Barnes <ruralmyth@gmail.com>
2017-01-04 20:49:49 +0000

    Kotlin: etl, more idomatic solution

--- kotlin/etl/src/main/kotlin/ETL.kt
+++ kotlin/etl/src/main/kotlin/ETL.kt
@@ -1,10 +1,8 @@
 object ETL {
     fun  transform(old: Map<Int, List<String>>): Map<String, Int> {
         return old.entries.flatMap {
-            entry ->
-            entry.value.map {
-                word ->
-                Pair(word.toLowerCase(), entry.key)
+            entry -> entry.value.map {
+                it.toLowerCase() to entry.key
             }
         }.toMap()
     }
