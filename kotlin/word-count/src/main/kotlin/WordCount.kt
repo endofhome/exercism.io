65edcd65a898b61740482769648b363e98a167c6
Tom Barnes <ruralmyth@gmail.com>
2017-01-06 23:54:47 +0000

    Kotlin: word-count, all tests passing. Think I can do better though...

--- kotlin/word-count/src/main/kotlin/WordCount.kt
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -4,7 +4,7 @@ object WordCount {
                 .toLowerCase()
                 .split(" ")
                 .map {
-                    it.replace(Regex("[:!&@$%^&,]"), "")
+                    it.replace(Regex("[:!&@$%^&,.]"), "")
                 }
                 .filter(String::isNotEmpty)
         return words.map{
