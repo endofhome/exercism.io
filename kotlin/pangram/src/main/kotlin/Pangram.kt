9fffadf2c2a5f843bb628762e8e417b6f8f002d1
Tom Barnes <ruralmyth@gmail.com>
2017-01-10 22:50:50 +0000

    Kotlin: pangram, more idiomatic solution

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -1,9 +1,7 @@
 object Pangrams {
     fun isPangram(sentence : String): Boolean {
-        val charCountMap = CharRange('a', 'z').map {
-            it to sentence.toLowerCase().contains(it)
-        }.toMap()
-
-        return charCountMap.values.all { it }
+        return ('a'..'z').all {
+            sentence.contains(it, ignoreCase = true)
+        }
     }
 }
