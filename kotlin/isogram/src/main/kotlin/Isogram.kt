2ea069b235dd32a331c95535f8205aa360919610
Tom Barnes <ruralmyth@gmail.com>
2017-01-11 23:15:42 +0000

    Kotlin: isogram, all tests passing

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,7 +1,14 @@
 class Isogram {
     companion object {
         fun isIsogram(input: String): Boolean {
-            return true
+            return input.toLowerCase()
+                    .toList()
+                    .filterNot {
+                        it == '-' || it == ' '
+                    }
+                    .groupBy { it }
+                    .mapValues { it.value.size }
+                    .all { it.value == 1 }
         }
     }
 }
