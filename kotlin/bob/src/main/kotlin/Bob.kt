37a6de61dc69459c035220c2a6c232eecea002ec
Tom Barnes <ruralmyth@gmail.com>
2017-01-16 21:44:24 +0000

    Kotlin: bob, more idiomatic use of 'when' and a String extension function

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,13 +1,13 @@
 object Bob {
-    fun hey(input: String): String {
-        return if (input.contains(Regex("[A-Z]")) && input == input.toUpperCase()) {
-           "Whoa, chill out!"
-        } else if (input.endsWith("?")) {
-            "Sure."
-        } else if (input.isBlank()) {
-            "Fine. Be that way!"
-        } else {
-            "Whatever."
-        }
+    fun hey(input: String): String =
+            when {
+                input.isShouting() -> "Whoa, chill out!"
+                input.endsWith("?") -> "Sure."
+                input.isBlank() -> "Fine. Be that way!"
+                else -> "Whatever."
+            }
+
+    private fun String.isShouting(): Boolean {
+        return (this.contains(Regex("[A-Z]")) && this == this.toUpperCase())
     }
 }
