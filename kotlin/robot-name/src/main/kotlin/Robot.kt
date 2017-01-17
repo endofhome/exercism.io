2c26ed2b58cb23e5b89df898b0b16bbd1dd71654
Tom Barnes <tom.barnes@springer.com>
2017-01-17 18:59:23 +0000

    Kotlin: robot-name, test #2 'differentRobotsHaveDifferentNames' passing

--- kotlin/robot-name/src/main/kotlin/Robot.kt
+++ kotlin/robot-name/src/main/kotlin/Robot.kt
@@ -1,5 +1,15 @@
 class Robot {
-    val name = "AB123"
+    val name = initialiseName()
+
+    private fun initialiseName(): String {
+        val letters = ('A'..'Z').toList()
+        fun letter() = letters[(Math.random() * letters.size).toInt()].toString()
+
+        val numbers = (0..9).toList()
+        fun number() = numbers[(Math.random() * numbers.size).toInt()].toString()
+
+        return letter() + letter() + number() + number() + number()
+    }
 
     fun reset() {
     }
