87debc25c077d42f72e07ee38fa000930fbf5550
Tom Barnes <tom.barnes@springer.com>
2017-01-17 19:47:36 +0000

    Kotlin: robot-name, prevent the same name being chosen twice. I think! How to test this?

--- kotlin/robot-name/src/main/kotlin/Robot.kt
+++ kotlin/robot-name/src/main/kotlin/Robot.kt
@@ -1,15 +1,22 @@
 class Robot {
+    val letters = ('A'..'Z').toList()
+    val numbers = (0..9).toList()
+    var previousNames = mutableSetOf<String>()
     var name = generateName()
 
     private fun generateName(): String {
-        val letters = ('A'..'Z').toList()
-        fun letter() = letters[(Math.random() * letters.size).toInt()].toString()
+        val proposedName = letter() + letter() + number() + number() + number()
 
-        val numbers = (0..9).toList()
-        fun number() = numbers[(Math.random() * numbers.size).toInt()].toString()
-
-        return letter() + letter() + number() + number() + number()
+        if (previousNames.contains(proposedName)) {
+            return generateName()
+        }
+        previousNames.add(proposedName)
+        return proposedName
     }
+
+    private fun letter() = letters[(Math.random() * letters.size).toInt()].toString()
+
+    private fun number() = numbers[(Math.random() * numbers.size).toInt()].toString()
 
     fun reset() {
         name = generateName()
