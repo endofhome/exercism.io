66e05018c9f456f276bc0699608950ae70a1562c
Tom Barnes <ruralmyth@gmail.com>
2017-01-18 22:57:30 +0000

    Kotlin: grade-school, compiling and test #1 'startsWithNoStudents' passing. Renamed School to GradeSchool because, yet again, the IDE is complaining - not recognising School.kt as a Kotlin file - and I don't know why

---
+++ kotlin/grade-school/src/main/kotlin/GradeSchool.kt
@@ -0,0 +1,6 @@
+class School {
+    fun db(): Map<Int, List<String>> = mapOf()
+    fun add(s: String, i: Int) {}
+    fun grade(i: Int): List<String> = listOf()
+    fun sort(): Map<Int, List<String>> = mapOf()
+}
