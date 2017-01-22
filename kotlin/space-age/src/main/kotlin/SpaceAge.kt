5e6272bbc10eb05df3a6205fcf5881ad1fb65ce0
Tom Barnes <ruralmyth@gmail.com>
2017-01-22 12:42:01 +0000

    Kotlin: space-age, test #1 'ageInSeconds' passing

---
+++ kotlin/space-age/src/main/kotlin/SpaceAge.kt
@@ -0,0 +1,11 @@
+class SpaceAge(age: Number) {
+    val seconds: Int = age.toInt()
+    fun onEarth(): Double = 0.0
+    fun onMercury(): Double = 0.0
+    fun onVenus(): Double = 0.0
+    fun onMars(): Double = 0.0
+    fun onJupiter(): Double = 0.0
+    fun onSaturn(): Double = 0.0
+    fun onUranus(): Double = 0.0
+    fun onNeptune(): Double = 0.0
+}
