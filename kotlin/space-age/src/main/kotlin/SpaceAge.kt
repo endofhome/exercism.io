40dd8c7469107fb9382aadb6de7a7385cca0a36c
Tom Barnes <ruralmyth@gmail.com>
2017-01-22 18:14:47 +0000

    Kotlin: space-age, test #4 'ageOnVenus' passing

--- kotlin/space-age/src/main/kotlin/SpaceAge.kt
+++ kotlin/space-age/src/main/kotlin/SpaceAge.kt
@@ -2,13 +2,14 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 class SpaceAge(age: Number) {
-    val seconds: Int = age.toInt()
+    val seconds = age.toInt()
+    private val preciseEarthAge = seconds.div(31557600.0)
 
-    fun onEarth() = format(seconds.div(31557600.0))
+    fun onEarth() = format(preciseEarthAge)
 
-    fun onMercury() = format(onEarth().div(0.2408467))
+    fun onMercury() = format(preciseEarthAge.div(0.2408467))
 
-    fun onVenus(): Double = 0.0
+    fun onVenus() = format(preciseEarthAge.div(0.61519726))
 
     fun onMars(): Double = 0.0
 
@@ -21,6 +22,6 @@ class SpaceAge(age: Number) {
     fun onNeptune(): Double = 0.0
 
     private fun format(highPrecisionNumber: Double): Double {
-        return BigDecimal(highPrecisionNumber).setScale(2, RoundingMode.HALF_DOWN).toDouble()
+        return BigDecimal(highPrecisionNumber).setScale(2, RoundingMode.HALF_UP).toDouble()
     }
 }
--- kotlin/space-age/src/test/kotlin/SpaceAgeTest.kt
+++ kotlin/space-age/src/test/kotlin/SpaceAgeTest.kt
@@ -26,7 +26,6 @@ class SpaceAgeTest {
         assertEquals(280.88, age.onMercury())
     }
 
-    @Ignore
     @Test
     fun ageOnVenus() {
         val age = SpaceAge(189839836)
