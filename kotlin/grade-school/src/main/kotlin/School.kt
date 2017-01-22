49925f31d185d8890de9f13bc5f39b999c3f9925
Tom Barnes <ruralmyth@gmail.com>
2017-01-22 12:13:39 +0000

    Kotlin: grade-school, much more concise re-assignment of db which also uses the grade method

--- kotlin/grade-school/src/main/kotlin/School.kt
+++ kotlin/grade-school/src/main/kotlin/School.kt
@@ -1,29 +1,10 @@
-import java.util.HashMap
-
 class School {
     private var db: Map<Int, List<String>> = emptyMap()
 
     fun db() = db
 
-    fun add(name: String, grade: Int) {
-        when (db.keys.contains(grade)) {
-            true -> addStudentForExistingGrade(name, grade)
-            false -> addStudentForNewGrade(name, grade)
-        }
-    }
-
-    private fun addStudentForExistingGrade(name: String, grade: Int) {
-        val localDb = db as MutableMap<Int, List<String>>
-        val studentsForGrade: MutableList<String> = localDb.get(grade).orEmpty().toMutableList()
-        studentsForGrade.add(name)
-        localDb[grade] = studentsForGrade
-        db = localDb
-    }
-
-    private fun addStudentForNewGrade(name: String, grade: Int) {
-        val localDb: MutableMap<Int, List<String>> = HashMap(db)
-        localDb[grade] = listOf(name)
-        db = localDb
+    fun add(studentName: String, studentGrade: Int) {
+        db += studentGrade to (grade(studentGrade) + studentName)
     }
 
     fun grade(grade: Int): List<String> {
