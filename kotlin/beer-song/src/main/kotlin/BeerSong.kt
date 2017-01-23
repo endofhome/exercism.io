95fdc8b8e643da3d6484241c9fdfc658c333d6ad
Tom Barnes <ruralmyth@gmail.com>
2017-01-23 00:16:54 +0000

    Kotlin: beer-song, test #1 'verse0' passing

---
+++ kotlin/beer-song/src/main/kotlin/BeerSong.kt
@@ -0,0 +1,9 @@
+object BeerSong {
+    val lyrics: String = ""
+
+    fun verse(number: Int): String {
+        return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    }
+
+    fun verses(startNumber: Int, endNumber: Int): String = ""
+}
