cce87a25e51d07514d549b0a70cb8fcdd3c233b5
Tom Barnes <ruralmyth@gmail.com>
2017-02-01 23:12:12 +0000

    Kotlin: largest-series-product, passing first 8 parameterised tests

--- kotlin/largest-series-product/src/main/kotlin/Series.kt
+++ kotlin/largest-series-product/src/main/kotlin/Series.kt
@@ -1,5 +1,17 @@
-class Series(input: String) {
+class Series(val input: String) {
     fun getLargestProduct(span: Int): Long {
-        return 72
+        val inputAsListOfLong = input.map { it.toString().toLong() }.toList()
+        return inputAsListOfLong.mapIndexed { i, l ->
+            inputAsListOfLong
+                    .slice(i.rangeTo(upperForRange(i, span)))
+                    .reduce { L1, L2 -> L1 * L2 }
+        }.sorted().last()
+    }
+
+    private fun upperForRange(i: Int, span: Int): Int {
+        return when (i + span - 1 < input.length) {
+            true -> i + span -1
+            else -> upperForRange(i, span -1)
+        }
     }
 }
