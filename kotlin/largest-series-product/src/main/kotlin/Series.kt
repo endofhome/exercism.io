cb6cbdfdd057c0965dd9bc6b3272d621e88711f0
Tom Barnes <ruralmyth@gmail.com>
2017-02-01 23:42:39 +0000

    Kotlin: largest-series-product, all valid input parameterised tests passing, I think this could probably be a lot nicer though

--- kotlin/largest-series-product/src/main/kotlin/Series.kt
+++ kotlin/largest-series-product/src/main/kotlin/Series.kt
@@ -1,17 +1,18 @@
 class Series(val input: String) {
     fun getLargestProduct(span: Int): Long {
         val inputAsListOfLong = input.map { it.toString().toLong() }.toList()
-        return inputAsListOfLong.mapIndexed { i, l ->
+        val listOfProducts = inputAsListOfLong.mapIndexed { i, l ->
             inputAsListOfLong
-                    .slice(i.rangeTo(upperForRange(i, span)))
+                    .slice(appropriateRange(i, span))
                     .reduce { L1, L2 -> L1 * L2 }
-        }.sorted().last()
+        }.sorted()
+        return listOfProducts.getOrElse(listOfProducts.lastIndex, { 1L })
     }
 
-    private fun upperForRange(i: Int, span: Int): Int {
+    private fun appropriateRange(i: Int, span: Int): IntRange {
         return when (i + span - 1 < input.length) {
-            true -> i + span -1
-            else -> upperForRange(i, span -1)
+            true -> i.rangeTo(i + span -1)
+            else -> (input.lastIndex - (span -1)).rangeTo(input.lastIndex)
         }
     }
 }
