b9e429a54f0f880aff0e150b82a91ad622e44db6
Tom Barnes <ruralmyth@gmail.com>
2017-02-04 19:47:56 +0000

    Kotlin: roman-numerals, five parameterised tests passing

--- kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
+++ kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
@@ -1,7 +1,9 @@
 object RomanNumeral {
     fun value(input: Int): String {
-        return 1.rangeTo(input).map {
-            "I"
-        }.joinToString("")
+        return when (input) {
+            in 0..3 -> 1.rangeTo(input).map { "I" }.joinToString("")
+            4 -> "IV"
+            else -> "not yet supported"
+        }
     }
 }
