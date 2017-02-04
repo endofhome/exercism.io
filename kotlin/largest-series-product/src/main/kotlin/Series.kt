2286b38988d8d09cebe7e5fae3260b0b9b2c295e
Tom Barnes <ruralmyth@gmail.com>
2017-02-04 10:36:15 +0000

    Kotlin: largest-series-product, reorganising things and adding an early return if input is empty means this can be a great deal more concise, and probably more readable

--- kotlin/largest-series-product/src/main/kotlin/Series.kt
+++ kotlin/largest-series-product/src/main/kotlin/Series.kt
@@ -1,24 +1,19 @@
 class Series(val input: String) {
 
     init {
-        require(input.all { it.isDigit() })
+        require(input.all(Char::isDigit))
     }
 
     fun getLargestProduct(span: Int): Long {
         require(span in 0..input.length)
 
-        val inputAsListOfLong = input.map { it.toString().toLong() }.toList()
-        return inputAsListOfLong.mapIndexed { i, l ->
-            inputAsListOfLong
-                    .slice(appropriateRange(i, span))
+        if (input.isEmpty()) return 1
+
+        return (0..input.length - span).map {
+            i ->
+            input.slice(i..i + span - 1)
+                    .map { it.toString().toLong() }
                     .reduce { L1, L2 -> L1 * L2 }
         }.max() ?: 1
-    }
-
-    private fun appropriateRange(i: Int, span: Int): IntRange {
-        return when (i + span - 1 < input.length) {
-            true -> i.rangeTo(i + span - 1)
-            else -> (input.lastIndex - (span - 1)).rangeTo(input.lastIndex)
-        }
     }
 }
