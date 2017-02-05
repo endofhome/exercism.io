a1ac862ddbf64d5554ad25a9860923d502274be9
Tom Barnes <ruralmyth@gmail.com>
2017-02-05 18:27:12 +0000

    Kotlin: roman-numerals, eight parameterised tests passing - numbers up to 9 converted correctly

--- kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
+++ kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
@@ -1,20 +1,22 @@
 object RomanNumeral {
     private val ONE = "I"
     private val FIVE = "V"
+    private val TEN = "X"
 
     fun value(input: Int): String {
         return when (input) {
             in 0..3 -> addRomanOnes(input)
-            in 4..8 -> fiveBased(input)
+            in 4..8 -> fiveOrTenBased(input, FIVE)
+            in 9..13 -> fiveOrTenBased(input, TEN)
             else -> "not yet supported"
         }
     }
 
-    private fun fiveBased(input: Int): String {
+    private fun fiveOrTenBased(input: Int, base: String): String {
         return when {
-            input == 4 -> ONE + FIVE
-            input >= 5 -> FIVE + addRomanOnes(input.minus(5))
-            else -> throw IllegalArgumentException("not '5' based")
+            input == 4 || input == 9 -> ONE + base
+            input >= 5 -> base + addRomanOnes(input.minus(5))
+            else -> throw IllegalArgumentException("not '5' or '10' based")
         }
     }
 
