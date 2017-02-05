e850734d366556f713514f3bfb27d430cbbcf6e1
Tom Barnes <ruralmyth@gmail.com>
2017-02-05 18:19:02 +0000

    Kotlin: roman-numerals, seven parameterised tests passing

--- kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
+++ kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
@@ -1,9 +1,22 @@
 object RomanNumeral {
+    private val ONE = "I"
+    private val FIVE = "V"
+
     fun value(input: Int): String {
         return when (input) {
-            in 0..3 -> 1.rangeTo(input).map { "I" }.joinToString("")
-            4 -> "IV"
+            in 0..3 -> addRomanOnes(input)
+            in 4..8 -> fiveBased(input)
             else -> "not yet supported"
         }
     }
+
+    private fun fiveBased(input: Int): String {
+        return when {
+            input == 4 -> ONE + FIVE
+            input >= 5 -> FIVE + addRomanOnes(input.minus(5))
+            else -> throw IllegalArgumentException("not '5' based")
+        }
+    }
+
+    private fun addRomanOnes(input: Int) = 1.rangeTo(input).map { ONE }.joinToString("")
 }
