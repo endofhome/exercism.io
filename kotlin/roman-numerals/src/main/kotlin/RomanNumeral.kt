84f31f4be45aada88189cd2a8e4aefe68deaa726
Tom Barnes <ruralmyth@gmail.com>
2017-02-06 23:05:29 +0000

    Kotlin: roman-numerals, prefer to use an 'arabicToRoman' map

--- kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
+++ kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
@@ -1,24 +1,26 @@
 object RomanNumeral {
-    private val ONE = "I"
-    private val FIVE = "V"
-    private val TEN = "X"
+    private val arabicToRoman = mapOf(
+        1 to "I",
+        5 to "V",
+        10 to "X"
+    )
 
     fun value(input: Int): String {
         return when (input) {
             in 0..3 -> addRomanOnes(input)
-            in 4..8 -> fiveOrTenBased(input, FIVE)
-            in 9..13 -> fiveOrTenBased(input, TEN)
+            in 4..8 -> fiveOrTenBased(input, arabicToRoman[5])
+            in 9..13 -> fiveOrTenBased(input, arabicToRoman[10])
             else -> "not yet supported"
         }
     }
 
-    private fun fiveOrTenBased(input: Int, base: String): String {
+    private fun fiveOrTenBased(input: Int, base: String?): String {
         return when {
-            input == 4 || input == 9 -> ONE + base
+            input == 4 || input == 9 -> arabicToRoman[1] + base
             input >= 5 -> base + addRomanOnes(input.minus(5))
             else -> throw IllegalArgumentException("not '5' or '10' based")
         }
     }
 
-    private fun addRomanOnes(input: Int) = 1.rangeTo(input).map { ONE }.joinToString("")
+    private fun addRomanOnes(input: Int) = 1.rangeTo(input).map { arabicToRoman[1] }.joinToString("")
 }
