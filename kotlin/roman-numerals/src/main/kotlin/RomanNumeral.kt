4d8afdffae05ff2d9e4bf27373999886208c14e1
Tom Barnes <ruralmyth@gmail.com>
2017-02-09 22:47:00 +0000

    Kotlin: roman-numeral, feel like this might be a bit long-winded, but tidied it up a little anyway

--- kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
+++ kotlin/roman-numerals/src/main/kotlin/RomanNumeral.kt
@@ -17,54 +17,59 @@ object RomanNumeral {
         val inputString = input.toString()
 
         return inputString.mapIndexed { i, char ->
-            val itAsInt = getNumericValue(char)
-            val length = inputString.length
-
-            val numeralToConvert = when (i) {
-                1 -> itAsInt * conversionFactor(length - 1)
-                2 -> itAsInt * conversionFactor(length - 2)
-                3 -> itAsInt * conversionFactor(length - 3)
-                else -> itAsInt * conversionFactor(length)
-            }
-
-            arabicToRoman[numeralToConvert] ?: (getRoman(numeralToConvert))
+            val arabicNumeral = numeralToConvert(char, i, inputString)
+            arabicToRoman[arabicNumeral] ?: (composeRoman(arabicNumeral))
         }.joinToString("")
     }
 
-    private fun conversionFactor(input: Int): Int {
-        return when (input) {
-            2 -> 10
-            3 -> 100
-            4 -> 1000
-            else -> 1
+    private fun numeralToConvert(char: Char, index: Int, inputString: String): Int {
+        val charAsInt = getNumericValue(char)
+        val length = inputString.length
+
+        return when (index) {
+            0 -> charAsInt * conversionFactor(length)
+            1 -> charAsInt * conversionFactor(length - 1)
+            2 -> charAsInt * conversionFactor(length - 2)
+            3 -> charAsInt * conversionFactor(length - 3)
+            else -> throw IllegalArgumentException("number must be below 3000.")
         }
     }
 
-    private fun getRoman(input: Int): String {
+    private fun composeRoman(input: Int): String {
         val firstDigit = getNumericValue(input.toString().toList().first())
         return when (firstDigit) {
-            in 0..3 -> addRomanOnes(firstDigit, conversionFactor(input.toString().length))
-            in 4..8 -> fiveOrTenBasedRoman(firstDigit, 5 * conversionFactor(input.toString().length))
-            9 -> fiveOrTenBasedRoman(firstDigit, 10 * conversionFactor(input.toString().length))
-            else -> ""
+            in 0..3 -> addPowersOfTenToRoman(firstDigit, conversionFactor(input.toString().length))
+            in 4..8 -> addFiveOrTenBasedRoman(firstDigit, 5 * conversionFactor(input.toString().length))
+            9 -> addFiveOrTenBasedRoman(firstDigit, 10 * conversionFactor(input.toString().length))
+            else -> throw IllegalArgumentException("input must be between 0 and 9")
         }
     }
 
-    private fun fiveOrTenBasedRoman(firstDigit: Int, arabicBase: Int): String {
+    private fun addPowersOfTenToRoman(numberOfOnes: Int, conversionFactor: Int) = 1.rangeTo(numberOfOnes).map { arabicToRoman[conversionFactor] }.joinToString("")
+
+    private fun addFiveOrTenBasedRoman(firstDigit: Int, arabicBase: Int): String {
         val base = arabicToRoman[arabicBase]
         val prefix = arabicToRoman[arabicBase / fiveOrTen(arabicBase)]
         return when {
             firstDigit == 4 || firstDigit == 9 -> prefix + base
-            firstDigit >= 5 -> base + addRomanOnes(firstDigit.minus(5), conversionFactor(arabicBase.toString().length))
-            else -> throw IllegalArgumentException("not '5' or '10' based")
+            firstDigit >= 5 -> base + addPowersOfTenToRoman(firstDigit.minus(5), conversionFactor(arabicBase.toString().length))
+            else -> throw IllegalArgumentException("not a '5' or '10' based roman numeral")
+        }
+    }
+
+    private fun conversionFactor(stringLength: Int): Int {
+        return when (stringLength) {
+            1 -> 1
+            2 -> 10
+            3 -> 100
+            4 -> 1000
+            else -> throw NoWhenBranchMatchedException("string length must be between § and 4")
         }
     }
 
     private fun fiveOrTen(arabicBase: Int): Int {
         val fiveOrTen = getNumericValue(arabicBase.toString().first())
         if (fiveOrTen == 1) return 10
-        return fiveOrTen
+        else return 5
     }
-
-    private fun addRomanOnes(numberOfOnes: Int, conversionFactor: Int) = 1.rangeTo(numberOfOnes).map { arabicToRoman[conversionFactor] }.joinToString("")
 }
