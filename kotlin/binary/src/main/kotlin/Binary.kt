549c06f7034efe3c2bd523390641db4e654824cb
Tom Barnes <ruralmyth@gmail.com>
2017-02-11 10:54:24 +0000

    Kotlin: binary, all tests passing. I wonder if this could be nicer, without the var...

--- kotlin/binary/src/main/kotlin/Binary.kt
+++ kotlin/binary/src/main/kotlin/Binary.kt
@@ -1,5 +1,7 @@
 object Binary {
     fun toDecimal(input: String): Int {
+        if (input.isNotBinaryNumber()) return 0
+
         var radix = 1
         return input.reversed().map {
             when (it) {
@@ -16,3 +18,5 @@ object Binary {
         }.sum()
     }
 }
+
+private fun String.isNotBinaryNumber(): Boolean = this.any { it != '0' && it != '1' }
