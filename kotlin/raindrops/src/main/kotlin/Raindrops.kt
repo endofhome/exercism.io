cec74efc67831356ec4253b2c87964baedc79cfe
Tom Barnes <ruralmyth@gmail.com>
2017-02-11 23:21:02 +0000

    Kotlin: raindrops, numbers with 5 as a prime factor

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -2,6 +2,7 @@ object Raindrops {
     fun convert(input: Int): String {
         return when {
             input % 3 == 0 -> "Pling"
+            input % 5 == 0 -> "Plang"
             else -> input.toString()
         }
     }
