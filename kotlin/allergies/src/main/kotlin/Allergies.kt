7a4fc25fa10f5724a33a2cce2d753bd7a5a16c8e
Tom Barnes <tom.barnes@springer.com>
2017-05-26 13:41:44 +0100

    Kotlin: allergies, attempt 2. allergic to lots of things

--- kotlin/allergies/src/main/kotlin/Allergies.kt
+++ kotlin/allergies/src/main/kotlin/Allergies.kt
@@ -1,5 +1,21 @@
+enum class Allergen(val score: Int) {
+    EGGS(1),
+    PEANUTS(2),
+    SHELLFISH(4),
+    STRAWBERRIES(8),
+    TOMATOES(16),
+    CHOCOLATE(32),
+    POLLEN(64),
+    CATS(128)
+}
+
 class Allergies(val allergyType: Int) {
-    fun isAllergicTo(allergen: Allergen): Boolean = false
+    fun isAllergicTo(allergen: Allergen): Boolean {
+        return when (allergyType) {
+            0 -> false
+            else -> true
+        }
+    }
 
     fun getList(): List<Any> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
