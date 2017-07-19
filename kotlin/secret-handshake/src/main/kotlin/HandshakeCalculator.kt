f880bae73b0b5612fa691542cf1df7d0231f337a
Tom Barnes <ruralmyth@gmail.com>
2017-07-19 18:01:31 +0100

    KotlinL: secret-handshake, refactoring

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -2,11 +2,11 @@ object HandshakeCalculator {
     fun calculateHandshake(int: Int): List<Signal> {
         val binary = Integer.toBinaryString(int)
         return (binary.length.let {
-            when {
-                it == 1 -> listOf(Signal.WINK)
-                it == 2 -> listOf(Signal.DOUBLE_BLINK)
-                it == 3 -> listOf(Signal.CLOSE_YOUR_EYES)
-                it == 4 -> listOf(Signal.JUMP)
+            when (it) {
+                1 -> listOf(Signal.WINK)
+                2 -> listOf(Signal.DOUBLE_BLINK)
+                3 -> listOf(Signal.CLOSE_YOUR_EYES)
+                4 -> listOf(Signal.JUMP)
                 else -> throw RuntimeException("Unexpected input")
             }
         })
