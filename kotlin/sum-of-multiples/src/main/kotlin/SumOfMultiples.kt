200fce384ad0e0792eb76de64558094959e06e1f
Tom Barnes <ruralmyth@gmail.com>
2017-07-20 09:09:32 +0100

    Kotlin: sum-of-multiples,  passing

--- kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,9 +1,12 @@
 object SumOfMultiples {
-    fun sum(of: Set<Int>, upTo: Int): Int {
-        var result = 0
-        of.forEach {
-            if (it < upTo) { result += it }
-        }
-        return result
-    }
+    fun sum(of: Set<Int>, upTo: Int): Int = of.flatMap {
+        it.multiples(upTo)
+    }.toList().sum()
 }
+
+fun Int.multiples(upTo: Int) = (0..upTo -1).mapNotNull {
+    when (it % this == 0) {
+        true -> it
+        false -> null
+    }
+}.toList()
