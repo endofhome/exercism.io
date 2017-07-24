db3c4d1f1a698d9354c6c47a884426c3631be546
Tom Barnes <ruralmyth@gmail.com>
2017-07-24 17:49:46 +0100

    Kotlin: grains, 'testSquare0IsInvalid' passing

--- kotlin/grains/src/main/kotlin/Board.kt
+++ kotlin/grains/src/main/kotlin/Board.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 
 object Board {
     fun  getGrainCountForSquare(squareNum: Int): BigInteger? {
+        require(squareNum in 1..64, { "Only integers between 1 and 64 (inclusive) are allowed" } )
         var counter = 1
         var grainCount = BigInteger.valueOf(1L)
         while (counter < squareNum) {
