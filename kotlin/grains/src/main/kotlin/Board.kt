2bf60fb47de9481a7ea776f6850113a9523b90c9
Tom Barnes <ruralmyth@gmail.com>
2017-07-24 08:53:00 +0100

    Kotlin: grains, compiling and 'testSquare1ContainsCorrectNumberOfGrains' passing

---
+++ kotlin/grains/src/main/kotlin/Board.kt
@@ -0,0 +1,6 @@
+import java.math.BigInteger
+
+object Board {
+    fun  getGrainCountForSquare(squareNum: Int): BigInteger? = BigInteger.ONE
+    fun getTotalGrainCount(): BigInteger = BigInteger("0")
+}
