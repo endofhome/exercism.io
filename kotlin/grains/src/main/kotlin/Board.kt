320f73f6c594be1e04299762da9a82ed91ed180b
Tom Barnes <ruralmyth@gmail.com>
2017-07-25 18:20:27 +0100

    Kotlin: grains, no need for a list

--- kotlin/grains/src/main/kotlin/Board.kt
+++ kotlin/grains/src/main/kotlin/Board.kt
@@ -4,11 +4,11 @@ object Board {
     fun  getGrainCountForSquare(squareNum: Int): BigInteger? {
         require(squareNum in 1..64, { "Only integers between 1 and 64 (inclusive) are allowed" } )
 
-        var grainCountList: List<BigInteger> = mutableListOf(BigInteger.ONE)
+        var grainsForSquare = BigInteger.ONE
         (2..squareNum).map {
-            grainCountList += grainCountList.last().multiply(BigInteger.valueOf(2L))
+            grainsForSquare = grainsForSquare.multiply(BigInteger.valueOf(2L))
         }
-        return grainCountList.last()
+        return grainsForSquare
     }
 
     fun getTotalGrainCount(): BigInteger = (1..64).mapNotNull {
