873d70fe87a42867dc2843de835ec1e755d0f598
Tom Barnes <ruralmyth@gmail.com>
2017-07-26 18:24:49 +0100

    Kotlin: grains... it's just maths.
    squareNum -1 because the first square is just 1 grain,
    multiplication only occurs from square 2 onward.

--- kotlin/grains/src/main/kotlin/Board.kt
+++ kotlin/grains/src/main/kotlin/Board.kt
@@ -4,11 +4,7 @@ object Board {
     fun  getGrainCountForSquare(squareNum: Int): BigInteger? {
         require(squareNum in 1..64, { "Only integers between 1 and 64 (inclusive) are allowed" } )
 
-        var grainsForSquare = BigInteger.ONE
-        (2..squareNum).map {
-            grainsForSquare = grainsForSquare.multiply(BigInteger.valueOf(2L))
-        }
-        return grainsForSquare
+        return BigInteger.valueOf(2L).pow(squareNum -1)
     }
 
     fun getTotalGrainCount(): BigInteger = (1..64).mapNotNull {
