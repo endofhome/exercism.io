795892f3ce3ad3fc6a75d300617edca74e76ab51
Tom Barnes <ruralmyth@gmail.com>
2017-07-28 08:57:13 +0100

    Kotlin: sieve, first cut of fully implemented algorithm

--- kotlin/sieve/src/main/kotlin/Sieve.kt
+++ kotlin/sieve/src/main/kotlin/Sieve.kt
@@ -1,3 +1,28 @@
 object Sieve {
-    fun primesUpTo(input: Int): List<Int> = listOf(2)
+    fun primesUpTo(input: Int): List<Int> {
+        val resultMap: MutableMap<Int, Status> =
+                (2..input).map { it to Status.UNMARKED }
+                        .toMap().toMutableMap()
+        resultMap.toSortedMap().firstKey().let {
+            markMultiples(it, input, resultMap)
+        }
+        return resultMap.filter { it.value == Status.UNMARKED }.keys.toList()
+    }
+
+    private fun markMultiples(root: Int, input: Int, resultMap: MutableMap<Int, Status>) {
+        resultMap.keys
+                .filter { resultMap[it] == Status.UNMARKED }
+                .map { toTest ->
+            val multiples = (input.downTo(toTest)).filter { it % toTest == 0 && it != toTest }
+            multiples.map { multiple ->
+                resultMap[multiple] = Status.MARKED
+            }
+        }
+        if (root < input) { markMultiples(root + 1, input, resultMap) }
+    }
+}
+
+enum class Status {
+    UNMARKED,
+    MARKED
 }
