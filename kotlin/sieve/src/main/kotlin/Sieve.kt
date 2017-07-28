d871d0b294ee268be0b830771203aa86ef38db02
Tom Barnes <ruralmyth@gmail.com>
2017-07-28 17:46:29 +0100

    Kotlin: sieve, refactoring

--- kotlin/sieve/src/main/kotlin/Sieve.kt
+++ kotlin/sieve/src/main/kotlin/Sieve.kt
@@ -1,19 +1,20 @@
 object Sieve {
     fun primesUpTo(input: Int): List<Int> {
-        val resultMap: MutableMap<Int, Status> =
-                (2..input).map { it to Status.UNMARKED }
-                        .toMap().toMutableMap()
+        val resultMap: MutableMap<Int, Status> = initialiseMap(input)
         resultMap.toSortedMap().firstKey().let {
             markMultiples(1, input, resultMap)
         }
         return resultMap.filter { it.value == Status.UNMARKED }.keys.toList()
     }
 
+    private fun initialiseMap(input: Int) = (2..input).map { it to Status.UNMARKED }
+            .toMap().toMutableMap()
+
     private fun markMultiples(step: Int, input: Int, resultMap: MutableMap<Int, Status>) {
         resultMap.keys
                 .filter { resultMap[it] == Status.UNMARKED }
-                .map { toTest ->
-            val multiples = (input.downTo(toTest)).filter { it % toTest == 0 && it != toTest }
+                .map { numToTest ->
+            val multiples = (input.downTo(numToTest)).filter { it % numToTest == 0 && it != numToTest }
             multiples.map { multiple ->
                 resultMap[multiple] = Status.MARKED
             }
