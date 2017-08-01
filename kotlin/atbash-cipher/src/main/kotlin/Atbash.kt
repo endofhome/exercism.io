84c2743dfe9af4c62fbb65a72f2c19cfefaa4c77
Tom Barnes <ruralmyth@gmail.com>
2017-08-01 21:43:18 +0100

    Kotlin: atbash-cipher, can encode 'mindblowingly'

--- kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
+++ kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
@@ -7,10 +7,29 @@ object Atbash {
         return input.map {
             val charPos = alphabet.indexOf(it.toLowerCase())
             alphabet.reversed()[charPos]
-        }.toList().joinToString("")
+        }.toList().joinToString("").maxFiveCharsPerWord()
     }
 
     fun decode(input: String): String {
         return ""
     }
+}
+
+private fun String.maxFiveCharsPerWord(): String {
+    val listOfFives = this.listOfFives()
+    return listOfFives.joinToString(" ")
+}
+
+private fun  String.listOfFives(): List<String> {
+    val workingList = this.toMutableList()
+    val outputList = mutableListOf<List<Char>>()
+    while (workingList.size > 0) {
+        outputList.add(workingList.take(5))
+        (0..4).map {
+            if (workingList.size > 0) {
+                workingList.removeAt(0)
+            }
+        }
+    }
+    return outputList.map { it.joinToString("") }
 }
