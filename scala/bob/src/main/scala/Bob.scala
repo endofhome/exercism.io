1a7acfb92dd05cc9e2bc2476ad08fc84eecaf11e
Tom Barnes <tom.barnes@springer.com>
2017-08-04 14:22:16 +0100

    Scala: bob, an acceptable way to do pattern matching that isn't fugly
    I might still prefer 98f0836cf85ae1150f3d0b6c790d0e86f0eca64f though.
    The jury is out.

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,14 +1,28 @@
 object Bob {
   def response(statement: String): String = {
-    if (statement.isShouting) "Whoa, chill out!"
-    else if (statement.trim.endsWith("?")) "Sure."
-    else if (statement.replaceAll("\\s*", "") .isEmpty) "Fine. Be that way!"
-    else "Whatever."
+    statement match {
+      case Shouting(_) => "Whoa, chill out!"
+      case Question(_) => "Sure."
+      case Silence(_) => "Fine. Be that way!"
+      case _ => "Whatever."
+    }
   }
 
-  private implicit class StringExtensions(val s: String){
-    def isShouting: Boolean = {
-      s == s.toUpperCase() && s.replaceAll("[a-zA-Z]", "") != s
+  private object Shouting {
+    def unapply(shout: String): Option[String] = {
+      if (shout == shout.toUpperCase() && shout.replaceAll("[a-zA-Z]", "") != shout) Some(shout) else None
+    }
+  }
+
+  private object Question {
+    def unapply(question: String): Option[String] = {
+      if (question.trim.endsWith("?")) Some(question) else None
+    }
+  }
+
+  private object Silence {
+    def unapply(silence: String): Option[String] = {
+      if (silence.replaceAll("\\s*", "").isEmpty) Some(silence) else None
     }
   }
 }
