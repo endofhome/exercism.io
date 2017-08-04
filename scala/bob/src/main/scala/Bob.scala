758813cf33e5dc9e3f6cac4fd4a173c0cc1d3576
Tom Barnes <tom.barnes@springer.com>
2017-08-04 11:56:39 +0100

    Scala: bob, revert this madness. If/else is more concise

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,11 +1,9 @@
 object Bob {
   def response(statement: String): String = {
-    statement match {
-      case s if s.isShouting => "Whoa, chill out!"
-      case s if s.trim.endsWith("?") => "Sure."
-      case s if s.replaceAll("\\s*", "").isEmpty => "Fine. Be that way!"
-      case _ => "Whatever."
-    }
+    if (statement.isShouting) "Whoa, chill out!"
+    else if (statement.trim.endsWith("?")) "Sure."
+    else if (statement.replaceAll("\\s*", "") .isEmpty) "Fine. Be that way!"
+    else "Whatever."
   }
 
   implicit class StringExtensions(val s: String){
