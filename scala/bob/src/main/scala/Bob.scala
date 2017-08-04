f9db21c99d4a95d203a3a6a28eb49ac46a80cb42
Tom Barnes <tom.barnes@springer.com>
2017-08-04 11:32:08 +0100

    Scala: bob, introduce implicit class for isShouting
    I want to write a Kotlin-style extension method to improve
    readability. This is the 'best' way I have found - not sure yet
    on the downsides. Am approaching with caution...

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,12 +1,14 @@
 object Bob {
   def response(statement: String): String = {
-    if (isShouting(statement)) "Whoa, chill out!"
+    if (statement.isShouting) "Whoa, chill out!"
     else if (statement.trim.endsWith("?")) "Sure."
     else if (statement.replaceAll("\\s*", "") .isEmpty) "Fine. Be that way!"
     else "Whatever."
   }
 
-  private def isShouting(statement: String): Boolean = {
-    statement == statement.toUpperCase() & statement.replaceAll("[a-zA-Z]", "") != statement
+  implicit class StringExtensions(val s: String){
+    def isShouting: Boolean = {
+      s == s.toUpperCase() & s.replaceAll("[a-zA-Z]", "") != s
+    }
   }
 }
