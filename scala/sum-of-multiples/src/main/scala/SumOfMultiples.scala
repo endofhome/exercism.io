dc2b9b66552f5b414ad26d526d51d060d9512f65
Tom Barnes <ruralmyth@gmail.com>
2017-08-22 09:03:54 +0100

    Scala: sum-of-multiples, all tests passing. Too Kotlinish?

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,7 +1,12 @@
 object SumOfMultiples {
   def sumOfMultiples(factors: Set[Int], limit: Int): Int = {
-    if (limit == 1) 0
-    else 3
+    factors.flatMap(f => f.multiples(limit)).sum
+  }
+
+  private implicit class IntExtensions(val subject: Int) {
+    def multiples(limit: Int): Set[Int] = {
+      (0 until limit).filter(i => i % subject == 0).toSet
+    }
   }
 }
 
