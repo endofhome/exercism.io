89d8121b02d8496abb2c9a07aa820876bbe2d03d
Tom Barnes <ruralmyth@gmail.com>
2017-08-22 09:07:58 +0100

    Scala: sum-of-multiples, better naming

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,10 +1,10 @@
 object SumOfMultiples {
   def sumOfMultiples(factors: Set[Int], limit: Int): Int = {
-    factors.flatMap(f => f.multiples(limit)).sum
+    factors.flatMap(f => f.multiplesUpTo(limit)).sum
   }
 
   private implicit class IntExtensions(val subject: Int) {
-    def multiples(limit: Int): Set[Int] = {
+    def multiplesUpTo(limit: Int): Set[Int] = {
       (0 until limit).filter(i => i % subject == 0).toSet
     }
   }
