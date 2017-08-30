0e926aed82655380dd08e8aaa6652686bef0ca72
Tom Barnes <ruralmyth@gmail.com>
2017-08-30 18:11:29 +0100

    Scala: space-age, 'age in seconds' passing

---
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -0,0 +1,10 @@
+case class SpaceAge(seconds: Long) {
+  val onEarth: Double = 0
+  val onMercury: Double = 0
+  val onVenus: Double = 0
+  val onMars: Double = 0
+  val onJupiter: Double = 0
+  val onSaturn: Double = 0
+  val onUranus: Double = 0
+  val onNeptune: Double = 0
+}
