a0d8c7c9cb4badb09e3adfce35c0d70d535e876c
Tom Barnes <ruralmyth@gmail.com>
2017-08-31 19:29:56 +0100

    Scala: space-age, longer but more accurate method naming

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -3,16 +3,16 @@ import scala.math.BigDecimal.RoundingMode.HALF_UP
 case class SpaceAge(seconds: Long) {
   private val PRECISE_EARTH_ORBITAL_PERIOD = seconds / 31557600d
 
-  val onEarth: Double = format(PRECISE_EARTH_ORBITAL_PERIOD)
-  val onMercury: Double = ageOnPlanet(0.2408467d)
-  val onVenus: Double = ageOnPlanet(0.61519726d)
-  val onMars: Double = ageOnPlanet(1.8808158)
-  val onJupiter: Double = ageOnPlanet(11.862615d)
-  val onSaturn: Double = ageOnPlanet(29.447498d)
-  val onUranus: Double = ageOnPlanet(84.016846d)
-  val onNeptune: Double = ageOnPlanet(164.79132d  )
+  val onEarth: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(1d)
+  val onMercury: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(0.2408467d)
+  val onVenus: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(0.61519726d)
+  val onMars: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(1.8808158)
+  val onJupiter: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(11.862615d)
+  val onSaturn: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(29.447498d)
+  val onUranus: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(84.016846d)
+  val onNeptune: Double = ageOnPlanetWhenEarthOrbitalPeriodIs(164.79132d)
 
-  private def ageOnPlanet(orbitalPeriodInEarthYears: Double) = {
+  private def ageOnPlanetWhenEarthOrbitalPeriodIs(orbitalPeriodInEarthYears: Double) = {
     format(PRECISE_EARTH_ORBITAL_PERIOD / orbitalPeriodInEarthYears)
   }
 
