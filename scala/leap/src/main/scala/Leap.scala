8602bef9735129872add8eb78bd0b6fc189251a9
Tom Barnes <tom.barnes@springer.com>
2017-09-01 15:42:55 +0100

    Scala: leap, more concise soluton

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,6 +1,5 @@
 object Leap {
   def leapYear(year: Int): Boolean = {
-    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) true
-    else false
+    year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
   }
 }
