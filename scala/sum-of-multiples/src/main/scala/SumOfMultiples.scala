6f4e0b9685cb8bd30442bd682a72e2aa9d0cac2d
Tom Barnes <tom.barnes@springer.com>
2017-09-01 15:37:28 +0100

    Scala: sum of multples, more concise but not icky.
    I wonder why we need so many different uses of `_` in Scala though.

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,11 +1,11 @@
 object SumOfMultiples {
   def sumOfMultiples(factors: Set[Int], limit: Int): Int = {
-    factors.flatMap(f => f.multiplesUpTo(limit)).sum
+    factors.flatMap(_.multiplesUpTo(limit)).sum
   }
 
   private implicit class IntExtensions(val subject: Int) {
     def multiplesUpTo(limit: Int): Set[Int] = {
-      (0 until limit).filter(i => i % subject == 0).toSet
+      (0 until limit).filter(_ % subject == 0).toSet
     }
   }
 }
