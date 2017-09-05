2e87a63310685241906677483fbec381b5a11d9e
Tom Barnes <ruralmyth@gmail.com>
2017-09-05 09:10:56 +0100

    Scala: accumulate, solution without using map()

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,5 +1,9 @@
+import scala.collection.mutable.ListBuffer
+
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
-    list.map( f(_) )
+  def accumulate[A, B](f: (A) => B, list: List[A]): List[B] = {
+    val processedElements = new ListBuffer[B]
+    list.foreach(processedElements += f(_))
+    processedElements.toList
   }
 }
