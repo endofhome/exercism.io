9be7642e87098d21fa2c3db6f915a69f76d27997
Tom Barnes <ruralmyth@gmail.com>
2017-09-05 18:24:55 +0100

    Scala: hamming, all tests passing

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,6 +1,9 @@
 object Hamming {
-  def distance(strandOne: String, strandTwo: String) = {
-    val zippedStrands = strandOne.zip(strandTwo)
-    Some(zippedStrands.count(pair => pair._1 != pair._2))
+  def distance(strandOne: String, strandTwo: String): Option[Integer] = {
+    if (strandOne.length != strandTwo.length) None
+    else {
+      val zippedStrands = strandOne.zip(strandTwo)
+      Some(zippedStrands.count(pair => pair._1 != pair._2))
+    }
   }
 }
