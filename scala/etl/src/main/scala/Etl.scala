788153d61aa8eda0ffef60c4042359c567737bb6
Tom Barnes <ruralmyth@gmail.com>
2017-09-06 09:01:04 +0100

    Scala: etl, wow - that was unexpected. All tests passing...

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,6 +1,5 @@
 object Etl {
   def transform(inputMap: Map[Int, Seq[String]]) = {
-    inputMap.flatMap(pair => pair._2
-      .map(_.toLowerCase -> pair._1))
+    inputMap.flatMap(pair => pair._2.map(_.toLowerCase -> pair._1))
   }
 }
