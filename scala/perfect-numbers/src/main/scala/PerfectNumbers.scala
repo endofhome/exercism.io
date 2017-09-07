81029d11a2ee03bdcd9370fcd9d88441dc5e835f
Tom Barnes <ruralmyth@gmail.com>
2017-09-07 09:17:15 +0100

    Scala: perfect-numbers, only calculate aliquot sum once

--- scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
+++ scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
@@ -3,12 +3,15 @@ import NumberType.NumberType
 object PerfectNumbers {
   def classify(int: Int): Either[String, NumberType] = {
     if (int <= 0) Left("Classification is only possible for natural numbers.")
-    else if (aliquotSum(int) == int) Right(NumberType.Perfect)
-    else if (aliquotSum(int) > int) Right(NumberType.Abundant)
-    else Right(NumberType.Deficient)
+    else {
+      val aliquotSum: Int = calcAliquotSum(int)
+      if (aliquotSum == int) Right(NumberType.Perfect)
+      else if (aliquotSum > int) Right(NumberType.Abundant)
+      else Right(NumberType.Deficient)
+    }
   }
 
-  def aliquotSum(int: Int): Int = int.factors.sum
+  def calcAliquotSum(int: Int): Int = int.factors.sum
 
   private implicit class IntExtensions(val i: Int) {
     def factors = (1 until i).filter(i % _ == 0).toList
