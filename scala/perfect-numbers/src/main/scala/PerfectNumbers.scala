37c578ba1b2e037faea4230ffb2af8c696508feb
Tom Barnes <ruralmyth@gmail.com>
2017-09-08 09:17:37 +0100

    Scala: perfect-numbers, using a more functional approach.
    Is it better? I'm not sure, though there are definitely
    things I like about it. Overkill, perhaps. Hmmmm.
    Probably would be nicer if the functions were actually part
    of the NumberType definition actually.
    The get() is also a bit smelly.

--- scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
+++ scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
@@ -1,17 +1,18 @@
 import NumberType.NumberType
 
 object PerfectNumbers {
+  type T = Int
+  type R = Int
+  val numberTypeMappings: Map[NumberType, (T, R) => Boolean] = Map(
+    (NumberType.Perfect, (T, R) => { T == R }),
+    (NumberType.Abundant, (T, R) => { T > R }),
+    (NumberType.Deficient, (T, R) => { T < R })
+  )
+
   def classify(int: Int): Either[String, NumberType] = {
     if (int <= 0) Left("Classification is only possible for natural numbers.")
-    else {
-      val aliquotSum: Int = calcAliquotSum(int)
-      if (aliquotSum == int) Right(NumberType.Perfect)
-      else if (aliquotSum > int) Right(NumberType.Abundant)
-      else Right(NumberType.Deficient)
-    }
+    else Right(numberTypeMappings.find(_._2(int.factors.sum, int)).get._1)
   }
-
-  def calcAliquotSum(int: Int): Int = int.factors.sum
 
   private implicit class IntExtensions(val i: Int) {
     def factors = (1 until i).filter(i % _ == 0).toList
