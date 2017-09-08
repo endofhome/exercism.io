1ce5de04c441acafb67f6202b15fb5b7c92fc037
Tom Barnes <ruralmyth@gmail.com>
2017-09-08 21:54:34 +0100

    Scala: perfect-numbers, aliquot sum should be on the definition

--- scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
+++ scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
@@ -1,20 +1,21 @@
 object PerfectNumbers {
-  def classify(int: Int): Either[String, NumberType.NumType] = {
-    if (int <= 0) Left("Classification is only possible for natural numbers.")
-    else Right(NumberType.values.find(_.definition(int.factors.sum, int)).get)
-  }
-
-  private implicit class IntExtensions(val i: Int) {
-    def factors = (1 until i).filter(i % _ == 0).toList
+  def classify(i: Int): Either[String, NumberType.NumType] = {
+    if (i <= 0) Left("Classification is only possible for natural numbers.")
+    else Right(NumberType.values.find(_.definition(i)).get)
   }
 }
 
 object NumberType {
   def values = List(Perfect, Abundant, Deficient)
 
-  sealed class NumType(val definition: (Int, Int) => Boolean)
+  sealed class NumType(val definition: (Int) => Boolean)
 
-  case object Perfect extends NumType((A, B) => { A == B })
-  case object Abundant extends NumType((A, B) => { A > B })
-  case object Deficient extends NumType((A, B) => { A < B })
+  case object Perfect extends NumType(i => { i.aliquotSum == i })
+  case object Abundant extends NumType(i => { i.aliquotSum > i })
+  case object Deficient extends NumType(i => { i.aliquotSum < i })
+
+  private implicit class IntExtensions(val i: Int) {
+    def aliquotSum = i.factors.sum
+    def factors = (1 until i).filter(i % _ == 0).toList
+  }
 }
