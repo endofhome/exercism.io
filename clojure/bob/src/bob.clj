9a6043272ecffc76f3688ece5edf009d0e6c48ec
Tom Barnes <ruralmyth@gmail.com>
2017-12-08 22:08:26 +0000

    Clojure: Bob, import clojure.string as str - a bit tidier

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,10 +1,11 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
 
 (defn response-for [sentence]
   (cond
-    (empty? (clojure.string/trim sentence))
+    (empty? (str/trim sentence))
       "Fine. Be that way!"
-    (and (= sentence (clojure.string/upper-case sentence)) (re-seq #"[a-z]+" (clojure.string/lower-case sentence)))
+    (and (= sentence (str/upper-case sentence)) (re-seq #"[a-z]+" (str/lower-case sentence)))
       "Whoa, chill out!"
     (= (str(last sentence)) "?")
       "Sure."
