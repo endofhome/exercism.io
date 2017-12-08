ba786e38d3dd51082ca50b818e90ce49c4fe4d48
Tom Barnes <ruralmyth@gmail.com>
2017-12-08 14:39:41 +0000

    Clojure: Hello world, another way

--- clojure/hello-world/src/hello_world.clj
+++ clojure/hello-world/src/hello_world.clj
@@ -1,9 +1,6 @@
 (ns hello-world)
 
-(def greeting "Hello, ")
-
-(defn hello [& names]
-  (if (empty? names)
-    (str greeting "World!")
-    (str greeting (first names) "!"))
+(defn hello
+  ([name] (str "Hello, " name "!"))
+  ([] (hello "World"))
 )
