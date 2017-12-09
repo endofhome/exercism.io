95abdcce854e0301cbc36c75b5bd97b485fdfeb3
Tom Barnes <ruralmyth@gmail.com>
2017-12-09 23:42:23 +0000

    Clojure: run-length-encoding, 'encode-empty-string' passing

---
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -0,0 +1,12 @@
+(ns run-length-encoding)
+
+(defn run-length-encode
+  "encodes a string with run-length-encoding"
+  [s]
+  ""
+  )
+
+(defn run-length-decode
+  "decodes a run-length-encoded string"
+  [s]
+  )
