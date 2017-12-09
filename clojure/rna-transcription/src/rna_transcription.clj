664ebfc7258286ad6a4fb0cba94ce521bc9b9b06
Tom Barnes <ruralmyth@gmail.com>
2017-12-09 18:57:01 +0000

    Clojure: rna-transaction, 'transcribes-guanine-to-cytosine' passing

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,3 +1,7 @@
 (ns rna-transcription)
 
-(defn to-rna [dna] "G")
+(defn to-rna [dna]
+  (cond
+    (= (str dna) "C") "G"
+    (= (str dna) "G") "C")
+)
--- clojure/rna-transcription/test/rna_transcription_test.clj
+++ clojure/rna-transcription/test/rna_transcription_test.clj
@@ -5,9 +5,9 @@
 (deftest transcribes-cytosine-to-guanine
   (is (= "G" (rna-transcription/to-rna "C"))))
 
-;(deftest transcribes-guanine-to-cytosine
-;  (is (= "C" (rna-transcription/to-rna "G"))))
-;
+(deftest transcribes-guanine-to-cytosine
+  (is (= "C" (rna-transcription/to-rna "G"))))
+
 ;(deftest transcribes-adenine-to-uracil
 ;  (is (= "U" (rna-transcription/to-rna "A"))))
 ;
