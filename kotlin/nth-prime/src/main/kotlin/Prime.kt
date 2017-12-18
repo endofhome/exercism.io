6f9103e91215e1ecd6ac1e7eac5e59a2d4e447da
Tom Barnes <ruralmyth@gmail.com>
2017-12-18 22:03:59 +0000

    Kotlin: nth-prime, 'sixthPrime' passing

--- kotlin/nth-prime/src/main/kotlin/Prime.kt
+++ kotlin/nth-prime/src/main/kotlin/Prime.kt
@@ -1,13 +1,13 @@
 object Prime {
     fun nth(i: Int): Int {
-        return primesUpToNth(i).last()
+        var counter = 2
+        return generateSequence { (counter++) }
+                .filter { it.isPrime() }
+                .take(i).last()
     }
 
-    private fun primesUpToNth(i: Int): List<Int> {
-        return when (i) {
-            1 -> listOf(2)
-            2 -> listOf(2, 3)
-            else -> listOf(2, 3, 5, 7, 11, 13)
-        }
-    }
+    private fun Int.isPrime() =
+        (1..this).filter {
+            this % it == 0
+        } == listOf(1, this)
 }
