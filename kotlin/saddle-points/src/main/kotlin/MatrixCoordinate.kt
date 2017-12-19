2bf35222dc0df38490b0b127b35f1706f76ca76f
Tom Barnes <ruralmyth@gmail.com>
2017-12-19 18:04:38 +0000

    Kotlin: saddle-points, 'testCanIdentifyThatEmptyMatrixHasNoSaddlePoints' passing

--- kotlin/saddle-points/src/main/kotlin/MatrixCoordinate.kt
+++ kotlin/saddle-points/src/main/kotlin/MatrixCoordinate.kt
@@ -1,6 +1,9 @@
 class Matrix(data: MatrixData, val saddlePoints: Set<MatrixCoordinate> = data.calculateSaddlePoints())
 
-fun MatrixData.calculateSaddlePoints() = setOf(MatrixCoordinate(1, 0))
+fun MatrixData.calculateSaddlePoints() = when {
+    this.first().isEmpty() -> emptySet()
+    else                   -> setOf(MatrixCoordinate(1, 0))
+}
 
-data class MatrixCoordinate(val row: Int, val col: Int)
 typealias MatrixData = List<List<Int>>
+data class MatrixCoordinate(val row: Int, val col: Int)
