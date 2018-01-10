539b60be960ac5926c19a9c031f2205554441a78
Tom Barnes <ruralmyth@gmail.com>
2018-01-10 19:07:07 +0000

    Kotlin: pig-latin, 'thr' and 'sch' are treated as single consonants

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -2,9 +2,11 @@ object PigLatin {
     val vowels = listOf('a', 'e', 'i', 'o', 'u')
     val consonants = ('a'..'z').filterNot { it in vowels }
     val specialPairs = listOf("ch", "qu", "th")
+    val specialTriples = listOf("thr", "sch")
 
     fun translate(input: String): String = when {
         input.first() in vowels         -> input + "ay"
+        input.take(3) in specialTriples -> input.drop(3) + input.subSequence(0, 3) + "ay"
         input.take(2) in specialPairs -> input.drop(2) + input.subSequence(0, 2) + "ay"
         input.first() in consonants     -> input.drop(1) + input.first() + "ay"
         else          -> input
