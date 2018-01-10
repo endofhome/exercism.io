de7fb2170dc702d2742decaa6bee30f3c56370ab
Tom Barnes <ruralmyth@gmail.com>
2018-01-10 09:29:57 +0000

    Kotlin: pig-latin, handle basic consonants

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,10 +1,10 @@
 object PigLatin {
     val vowels = listOf('a', 'e', 'i', 'o', 'u')
+    val consonants = ('a'..'z').filterNot { it in vowels }
 
-    fun translate(input: String): String {
-        return when (input.first()) {
+    fun translate(input: String): String = when (input.first()) {
             in vowels -> input + "ay"
+            in consonants -> input.drop(1) + input.first() + "ay"
             else       -> input
-        }
     }
 }
