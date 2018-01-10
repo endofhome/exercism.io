ea1eb870520f0a8d583caba8122b2e172864ffa3
Tom Barnes <ruralmyth@gmail.com>
2018-01-10 18:56:53 +0000

    Kotlin: pig-latin, 'ch' is treated as a single consonant

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -2,9 +2,10 @@ object PigLatin {
     val vowels = listOf('a', 'e', 'i', 'o', 'u')
     val consonants = ('a'..'z').filterNot { it in vowels }
 
-    fun translate(input: String): String = when (input.first()) {
-            in vowels -> input + "ay"
-            in consonants -> input.drop(1) + input.first() + "ay"
-            else       -> input
+    fun translate(input: String): String = when {
+        input.first() in vowels     -> input + "ay"
+        input.take(2) == "ch"       -> input.drop(2) + input.subSequence(0, 2) + "ay"
+        input.first() in consonants -> input.drop(1) + input.first() + "ay"
+        else          -> input
     }
 }
