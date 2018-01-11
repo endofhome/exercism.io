836f4300a90458d720cedb6a90792d3e0f227e77
Tom Barnes <ruralmyth@gmail.com>
2018-01-11 17:12:38 +0000

    Kotlin: pig-latin, all tests passing, but some refactoring to be done

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -5,13 +5,18 @@ object PigLatin {
     val consonantTriples = listOf("thr", "sch")
     val vowelPairs = listOf("yt", "xr")
 
-    fun translate(input: String): String = when {
-        input.take(2) in vowelPairs       -> input + "ay"
-        input.first() in vowels           -> input + "ay"
-        input.drop(1).take(2) == "qu"     -> input.drop(3) + input.take(3) + "ay"
-        input.take(3) in consonantTriples -> input.drop(3) + input.subSequence(0, 3) + "ay"
-        input.take(2) in consonantPairs   -> input.drop(2) + input.subSequence(0, 2) + "ay"
-        input.first() in consonants       -> input.drop(1) + input.first() + "ay"
-        else          -> input
+    fun translate(input: String) =
+            input.split(" ")
+                 .map { translateSingleWord(it) }
+                 .joinToString(" ")
+
+    fun translateSingleWord(word: String): String = when {
+        word.take(2) in vowelPairs       -> word + "ay"
+        word.first() in vowels           -> word + "ay"
+        word.drop(1).take(2) == "qu"     -> word.drop(3) + word.take(3) + "ay"
+        word.take(3) in consonantTriples -> word.drop(3) + word.subSequence(0, 3) + "ay"
+        word.take(2) in consonantPairs   -> word.drop(2) + word.subSequence(0, 2) + "ay"
+        word.first() in consonants       -> word.drop(1) + word.first() + "ay"
+        else          -> word
     }
 }
