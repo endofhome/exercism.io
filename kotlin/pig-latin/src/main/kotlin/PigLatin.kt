fbd9cb4b01663f53d5d75e8c4368783bc34760b6
Tom Barnes <ruralmyth@gmail.com>
2018-01-11 17:04:22 +0000

    Kotlin: pig-latin, 'qu' preceded by any single character is handled correctly

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -8,6 +8,7 @@ object PigLatin {
     fun translate(input: String): String = when {
         input.take(2) in vowelPairs       -> input + "ay"
         input.first() in vowels           -> input + "ay"
+        input.drop(1).take(2) == "qu"     -> input.drop(3) + input.take(3) + "ay"
         input.take(3) in consonantTriples -> input.drop(3) + input.subSequence(0, 3) + "ay"
         input.take(2) in consonantPairs   -> input.drop(2) + input.subSequence(0, 2) + "ay"
         input.first() in consonants       -> input.drop(1) + input.first() + "ay"
