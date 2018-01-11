8e652bcbf22eaacbe86570544517ebc33db55e24
Tom Barnes <ruralmyth@gmail.com>
2018-01-11 17:25:16 +0000

    Kotlin: pig-latin, minor refactorings

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,22 +1,26 @@
 object PigLatin {
     val vowels = listOf('a', 'e', 'i', 'o', 'u')
     val consonants = ('a'..'z').filterNot { it in vowels }
-    val consonantPairs = listOf("ch", "qu", "th", "rh")
-    val consonantTriples = listOf("thr", "sch")
-    val vowelPairs = listOf("yt", "xr")
+    val specialConsonantPairs = listOf("ch", "qu", "th", "rh")
+    val specialConsonantTriples = listOf("thr", "sch")
+    val specialVowelPairs = listOf("yt", "xr")
 
     fun translate(input: String) =
             input.split(" ")
                  .map { translateSingleWord(it) }
                  .joinToString(" ")
 
-    fun translateSingleWord(word: String): String = when {
-        word.take(2) in vowelPairs       -> word + "ay"
-        word.first() in vowels           -> word + "ay"
-        word.drop(1).take(2) == "qu"     -> word.drop(3) + word.take(3) + "ay"
-        word.take(3) in consonantTriples -> word.drop(3) + word.subSequence(0, 3) + "ay"
-        word.take(2) in consonantPairs   -> word.drop(2) + word.subSequence(0, 2) + "ay"
-        word.first() in consonants       -> word.drop(1) + word.first() + "ay"
-        else          -> word
+    fun translateSingleWord(word: String): String {
+        val pigLatinEnding = "ay"
+
+        return when {
+            word.take(2) in specialVowelPairs       -> word + pigLatinEnding
+            word.first() in vowels                  -> word + pigLatinEnding
+            word.drop(1).take(2) == "qu"            -> word.drop(3) + word.take(3) + pigLatinEnding
+            word.take(3) in specialConsonantTriples -> word.drop(3) + word.subSequence(0, 3) + pigLatinEnding
+            word.take(2) in specialConsonantPairs   -> word.drop(2) + word.subSequence(0, 2) + pigLatinEnding
+            word.first() in consonants              -> word.drop(1) + word.first() + pigLatinEnding
+            else                                    -> word
+        }
     }
 }
