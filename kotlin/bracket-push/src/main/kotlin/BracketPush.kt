d005d74eb105bdaff76e1ea10e37cd7e4319fdf9
Tom Barnes <ruralmyth@gmail.com>
2018-02-05 18:07:50 +0000

    Kotlin: bracket-push, simple handling of square brackets

--- kotlin/bracket-push/src/main/kotlin/BracketPush.kt
+++ kotlin/bracket-push/src/main/kotlin/BracketPush.kt
@@ -1,3 +1,18 @@
 object BracketPush {
-    fun isValid(input: String) = true
+    object SquareBrackets {
+        val openingValue = '['
+        val closingValue = ']'
+        var open = 0
+    }
+
+    fun isValid(input: String): Boolean {
+        input.forEach { char ->
+            when (char) {
+                SquareBrackets.openingValue -> SquareBrackets.open++
+                SquareBrackets.closingValue -> SquareBrackets.open--
+            }
+        }
+
+        return SquareBrackets.open == 0
+    }
 }
