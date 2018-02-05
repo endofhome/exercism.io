ccffe2f567d991abb99dca32bc1c9354a36b17b8
Tom Barnes <ruralmyth@gmail.com>
2018-02-05 18:16:40 +0000

    Kotlin: bracket-push, simple handling of braces. Meh.

--- kotlin/bracket-push/src/main/kotlin/BracketPush.kt
+++ kotlin/bracket-push/src/main/kotlin/BracketPush.kt
@@ -3,16 +3,39 @@ object BracketPush {
         val openingValue = '['
         val closingValue = ']'
         var open = 0
+
+        fun reset() {
+            open = 0
+        }
+    }
+
+    object Braces {
+        val openingValue = '{'
+        val closingValue = '}'
+        var open = 0
+
+        fun reset() {
+            open = 0
+        }
     }
 
     fun isValid(input: String): Boolean {
+        resetCounts()
+
         input.forEach { char ->
             when (char) {
                 SquareBrackets.openingValue -> SquareBrackets.open++
                 SquareBrackets.closingValue -> SquareBrackets.open--
+                Braces.openingValue         -> Braces.open++
+                Braces.closingValue         -> Braces.open--
             }
         }
 
-        return SquareBrackets.open == 0
+        return (SquareBrackets.open == 0) && (Braces.open == 0)
+    }
+
+    private fun resetCounts() {
+        SquareBrackets.reset()
+        Braces.reset()
     }
 }
