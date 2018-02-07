6cb23da21f79038181c9819f3e879e1151d74667
Tom Barnes <ruralmyth@gmail.com>
2018-02-07 22:48:32 +0000

    Kotlin: bracket-push, remove duplication and improve flexibility of the code.

--- kotlin/bracket-push/src/main/kotlin/BracketPush.kt
+++ kotlin/bracket-push/src/main/kotlin/BracketPush.kt
@@ -1,13 +1,21 @@
-object BracketPush {
-    var openBrackets: MutableList<Char> = mutableListOf()
-    private val squareBrackets = Bracket(openingValue = '[', closingValue = ']')
-    private val braces = Bracket(openingValue = '{', closingValue = '}')
-    private val parentheses = Bracket(openingValue = '(', closingValue = ')')
+import BracketPush.Bracket.SquareBracket
+import BracketPush.Bracket.Braces
+import BracketPush.Bracket.Parentheses
 
-    class Bracket(val openingValue: Char = '[',
-                  val closingValue: Char = ']',
-                  var open: Int = 0
+object BracketPush {
+    private val bracketTypes: List<Bracket> = listOf(SquareBracket, Braces, Parentheses)
+    private var openBrackets: MutableList<Char> = mutableListOf()
+
+    sealed class Bracket(
+            val openingValue: Char,
+            val closingValue: Char,
+            var open: Int = 0
     ) {
+
+        object SquareBracket : Bracket(openingValue = '[', closingValue = ']')
+        object Braces : Bracket(openingValue = '{', closingValue = '}')
+        object Parentheses : Bracket(openingValue = '(', closingValue = ')')
+
         fun reset() {
             open = 0
         }
@@ -28,6 +36,13 @@ object BracketPush {
                 throw IllegalStateException("Cannot close brackets before they have been opened.")
             }
         }
+
+        fun process(char: Char) {
+            when (char) {
+                openingValue -> openBracket()
+                closingValue -> closeBracket()
+            }
+        }
     }
 
     fun isValid(input: String): Boolean {
@@ -35,25 +50,26 @@ object BracketPush {
 
         try {
             input.forEach { char ->
-                when (char) {
-                    squareBrackets.openingValue -> squareBrackets.openBracket()
-                    squareBrackets.closingValue -> squareBrackets.closeBracket()
-                    braces.openingValue         -> braces.openBracket()
-                    braces.closingValue         -> braces.closeBracket()
-                    parentheses.openingValue    -> parentheses.openBracket()
-                    parentheses.closingValue    -> parentheses.closeBracket()
+                val maybeBracket: Bracket? = analysed(char)
+                when (maybeBracket) {
+                    is Bracket -> maybeBracket.process(char)
                 }
             }
         } catch (e: IllegalStateException) {
             return false
         }
 
-        return (squareBrackets.open == 0) && (braces.open == 0) && (parentheses.open == 0)
+        return bracketTypes.all { it.open == 0 }
+    }
+
+    private fun analysed(char: Char): Bracket? {
+        val openingValues = BracketPush.bracketTypes.map { bracketType -> bracketType.openingValue to bracketType }
+        val closingValues = BracketPush.bracketTypes.map { bracketType -> bracketType.closingValue to bracketType }
+        val bracketCharacters = openingValues + closingValues
+        return bracketCharacters.find { it.first == char }?.second
     }
 
     private fun resetState() {
-        squareBrackets.reset()
-        braces.reset()
-        parentheses.reset()
+        bracketTypes.forEach { it.reset() }
     }
 }
