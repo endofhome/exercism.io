5a8ff0908f0c3376e6d611885406e2b4489b8ff2
Tom Barnes <ruralmyth@gmail.com>
2018-02-08 17:03:53 +0000

    Kotlin: bracket-push, attempt to separate the two classes. They are coupled in both directions via the Brackets class which I'm not very happy with.

--- kotlin/bracket-push/src/main/kotlin/BracketPush.kt
+++ kotlin/bracket-push/src/main/kotlin/BracketPush.kt
@@ -1,58 +1,19 @@
-import BracketPush.Bracket.SquareBracket
-import BracketPush.Bracket.Braces
-import BracketPush.Bracket.Parentheses
+import Brackets.SquareBrackets
+import Brackets.Braces
+import Brackets.Parentheses
 
 object BracketPush {
-    private val bracketTypes: List<Bracket> = listOf(SquareBracket, Braces, Parentheses)
+    private val bracketTypes: List<Brackets> = listOf(SquareBrackets, Braces, Parentheses)
     private var openBrackets: MutableList<Char> = mutableListOf()
-
-    sealed class Bracket(
-            val openingValue: Char,
-            val closingValue: Char,
-            var open: Int = 0
-    ) {
-
-        object SquareBracket : Bracket(openingValue = '[', closingValue = ']')
-        object Braces : Bracket(openingValue = '{', closingValue = '}')
-        object Parentheses : Bracket(openingValue = '(', closingValue = ')')
-
-        fun reset() {
-            open = 0
-        }
-
-        fun process(char: Char) {
-            when (char) {
-                openingValue -> openBracket()
-                closingValue -> closeBracket()
-            }
-        }
-
-        private fun openBracket() {
-            open++
-            openBrackets.add(openingValue)
-        }
-
-        private fun closeBracket() {
-            if (openBrackets.last() == openingValue) {
-                open--
-                openBrackets.removeAt(openBrackets.lastIndex)
-            } else {
-                throw IllegalStateException("Brackets are not nested correctly")
-            }
-            if (open < 0) {
-                throw IllegalStateException("Cannot close brackets before they have been opened.")
-            }
-        }
-    }
 
     fun isValid(input: String): Boolean {
         resetState()
 
         try {
             input.forEach { char ->
-                val maybeBracket: Bracket? = analysed(char)
+                val maybeBracket: Brackets? = analysed(char)
                 when (maybeBracket) {
-                    is Bracket -> maybeBracket.process(char)
+                    is Brackets -> maybeBracket.process(char)
                 }
             }
         } catch (e: IllegalStateException) {
@@ -62,7 +23,14 @@ object BracketPush {
         return bracketTypes.all { it.open == 0 }
     }
 
-    private fun analysed(char: Char): Bracket? {
+    fun addOpenBracket(char: Char) = openBrackets.add(char)
+
+    fun lastOpenBracket() = openBrackets.last()
+
+    fun removeLastOpenBracket() = openBrackets.removeAt(openBrackets.lastIndex)
+
+
+    private fun analysed(char: Char): Brackets? {
         val openingValues = BracketPush.bracketTypes.map { bracketType -> bracketType.openingValue to bracketType }
         val closingValues = BracketPush.bracketTypes.map { bracketType -> bracketType.closingValue to bracketType }
         val bracketCharacters = openingValues + closingValues
@@ -71,5 +39,44 @@ object BracketPush {
 
     private fun resetState() {
         bracketTypes.forEach { it.reset() }
+    }
+}
+
+sealed class Brackets(
+        val openingValue: Char,
+        val closingValue: Char,
+        var open: Int = 0,
+        private val bracketPush: BracketPush = BracketPush
+) {
+    object SquareBrackets : Brackets(openingValue = '[', closingValue = ']')
+    object Braces : Brackets(openingValue = '{', closingValue = '}')
+    object Parentheses : Brackets(openingValue = '(', closingValue = ')')
+
+    fun reset() {
+        open = 0
+    }
+
+    fun process(char: Char) {
+        when (char) {
+            openingValue -> openBracket()
+            closingValue -> closeBracket()
+        }
+    }
+
+    private fun openBracket() {
+        open++
+        bracketPush.addOpenBracket(openingValue)
+    }
+
+    private fun closeBracket() {
+        if (bracketPush.lastOpenBracket() == openingValue) {
+            open--
+            bracketPush.removeLastOpenBracket()
+        } else {
+            throw IllegalStateException("Brackets are not nested correctly")
+        }
+        if (open < 0) {
+            throw IllegalStateException("Cannot close brackets before they have been opened.")
+        }
     }
 }
