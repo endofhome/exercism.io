88917878856b7c8d1f81d8634450e4753350f8d3
Tom Barnsbury <ruralmyth@gmail.com>
2019-09-18 17:07:26 +0100

    Kotlin: rotational-cipher, refactoring

--- kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -2,6 +2,8 @@ import Case.UpperCase
 import Case.LowerCase
 
 class RotationalCipher(private val key: Int) {
+    private val cases = listOf(UpperCase, LowerCase)
+
     fun encode(input: String) =
         input.toCharArray().map { char ->
             when {
@@ -10,33 +12,29 @@ class RotationalCipher(private val key: Int) {
             }
         }.joinToString("")
 
+    private fun Char.rotateBy(key: Int): Char {
+        val charAsInt = this.toInt()
+        val rotatedChar = charAsInt + key
+
+        val rotatedCharAsInt = cases.find { case ->
+            charAsInt in case.charRange && rotatedChar > case.lastChar
+        }?.adjustInRange(rotatedChar) ?: rotatedChar
+
+        return rotatedCharAsInt.toChar()
+    }
 }
 
 interface RangeOfChars {
     val firstChar: Int
     val lastChar: Int
+    val charRange get() = firstChar..lastChar
 }
 
 sealed class Case : RangeOfChars {
     private val alphabetSize = 26
-    val charRange get() = firstChar..lastChar
     override val lastChar get() =  firstChar + alphabetSize - 1
     fun adjustInRange(i: Int) = i - alphabetSize
 
-
     object UpperCase : Case() { override val firstChar = 65 }
     object LowerCase : Case() { override val firstChar = 97 }
-}
-
-fun Char.rotateBy(key: Int): Char {
-    val cases = listOf(UpperCase, LowerCase)
-    val charAsInt = this.toInt()
-    val rotatedChar = charAsInt + key
-
-    val rotatedCharAsInt = cases.find {
-        case -> charAsInt in case.charRange && rotatedChar > case.lastChar
-    }?.adjustInRange(rotatedChar)
-        ?: rotatedChar
-
-    return rotatedCharAsInt.toChar()
 }
