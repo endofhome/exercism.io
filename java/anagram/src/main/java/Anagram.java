8a4bb1aa1049d9b81f2264f25744e00d9ea84062
Tom Barnsbury <ruralmyth@gmail.com>
2019-11-06 16:48:09 +0000

    Java: Anagram (2), simple multiple anagrams are supported

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,12 +1,25 @@
+import java.util.Arrays;
 import java.util.List;
 
-import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class Anagram {
+    private final String sortedSubject;
+
     public Anagram(String subject) {
+        sortedSubject = sortCharacters(subject);
     }
 
     public List<String> match(List<String> candidates) {
-        return emptyList();
+        return candidates.stream()
+                .filter(it -> sortCharacters(it).equals(this.sortedSubject))
+                .collect(toList());
+    }
+
+    private String sortCharacters(String subject) {
+        return Arrays.stream(subject.split(""))
+                .sorted()
+                .collect(joining());
     }
 }
