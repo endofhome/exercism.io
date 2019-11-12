e482af8fe750f88236b902fe331dbcc74b2bc999
Tom Barnsbury <ruralmyth@gmail.com>
2019-11-12 13:44:52 +0000

    Java: Meetup, Prefer enum with higher order functions

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -28,26 +28,12 @@ public class Meetup {
     }
 
     private IntStream possibleDays(MeetupSchedule schedule, YearMonth yearMonth) {
-        int firstDayOfWeek = firstDayOfWeek(schedule, yearMonth);
+        int firstDayOfWeek = schedule.firstDayOfWeek(yearMonth);
 
         return IntStream.range(firstDayOfWeek, firstDayOfWeek + 7);
     }
 
     private boolean isMonthDay(int dayNumber, YearMonth yearMonth, int i) {
         return yearMonth.atDay(i).getDayOfWeek() == DayOfWeek.of(dayNumber);
-    }
-
-    private int firstDayOfWeek(MeetupSchedule schedule, YearMonth yearMonth) {
-        int firstDayOfWeek;
-
-        if (schedule == MeetupSchedule.LAST) {
-            firstDayOfWeek = yearMonth.lengthOfMonth() - 6;
-        } else if (schedule == MeetupSchedule.TEENTH) {
-            firstDayOfWeek = 13;
-        } else {
-            firstDayOfWeek = (schedule.ordinal() * 7) + 1;
-        }
-
-        return firstDayOfWeek;
     }
 }
--- java/meetup/src/main/java/MeetupSchedule.java
+++ java/meetup/src/main/java/MeetupSchedule.java
@@ -1,8 +1,21 @@
+import java.time.YearMonth;
+import java.util.function.Function;
+
 public enum MeetupSchedule {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    LAST,
-    TEENTH
+    FIRST(x -> 1),
+    SECOND(x -> 8),
+    THIRD(x -> 15),
+    FOURTH(x -> 22),
+    LAST(yearMonth -> yearMonth.lengthOfMonth() - 6),
+    TEENTH(x -> 13);
+
+    private final Function<YearMonth, Integer> firstDayOfWeek;
+
+    MeetupSchedule(Function<YearMonth, Integer> firstDayOfWeek) {
+        this.firstDayOfWeek = firstDayOfWeek;
+    }
+
+    public int firstDayOfWeek(YearMonth yearMonth) {
+        return firstDayOfWeek.apply(yearMonth);
+    }
 }
