44b6a1bbb1bbba333e36e37c65268625e4de3980
Tom Barnsbury <ruralmyth@gmail.com>
2019-11-12 22:38:50 +0000

    Java: Pangrams #2, More efficient, though it seems the Java API is determined to make this more cryptic (with reliance on negation)

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -5,9 +5,10 @@ public class Pangrams {
     public static boolean isPangram(String sentence) {
         String formattedSentence = formatted(sentence);
 
-        return IntStream.range(97, 123)
-                .filter(c -> formattedSentence.contains(Character.toString((char) c)))
-                .count() == 26;
+        return ! IntStream.range(97, 123)
+                .filter(c -> ! formattedSentence.contains(Character.toString((char) c)))
+                .findAny()
+                .isPresent();
     }
 
     private static String formatted(String sentence) {
