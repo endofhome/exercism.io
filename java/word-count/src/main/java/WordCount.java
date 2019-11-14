b385d078c171ee8d2698d839987f2baa9fd4c9fd
Tom Barnsbury <ruralmyth@gmail.com>
2019-11-14 16:57:16 +0000

    Java: word-count #2, countOneOfEach passing

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,11 +1,16 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static java.util.Arrays.asList;
 
 class WordCount {
 
     Map<String,Integer> phrase(String phrase) {
         HashMap<String, Integer> result = new HashMap<>();
-        result.put(phrase, 1);
+        List<String> words = asList(phrase.split(" "));
+        words.forEach(word -> result.put(word, 1));
 
         return result;
     }
--- java/word-count/src/test/java/WordCountTest.java
+++ java/word-count/src/test/java/WordCountTest.java
@@ -24,7 +24,6 @@ public class WordCountTest {
         );
     }
 
-    @Ignore
     @Test
     public void countOneOfEach() {
         Map<String, Integer> actualWordCount;
